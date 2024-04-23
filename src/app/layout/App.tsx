4e00bb4bc89cf933eddc6d8bc70e2a46eafc0bf1
LyyHua <lyhuavanly@gmail.com>
2024-04-24 01:12:33 +0700

    I manage to add a footer through a painstaking process of researching tho it scale good for everything under 1920x1080 anything from that and beyond suck

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,5 +1,6 @@
 import { CarouselData } from "../../features/Image Carousel/CarouselData";
 import ImageCarousel from "../../features/Image Carousel/ImageCarousel";
+import PageCredit from "./footer/PageCredit";
 import NavBar from "./nav/NavBar";
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
     <div>
       <NavBar />
       <ImageCarousel image={CarouselData}/>
+      <PageCredit />
     </div>
   )
 }
