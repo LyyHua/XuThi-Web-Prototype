01a84b9de5c9ae3fbaa3b96260853671f9f13117
LyyHua <lyhuavanly@gmail.com>
2024-05-11 19:37:45 +0700

    I optimise the web so that it render faster and smoother

--- src/features/FinishShopping/FinishShopping.tsx
+++ src/features/FinishShopping/FinishShopping.tsx
@@ -1,48 +1,6 @@
-import { useEffect } from "react";
 import { Image, Container } from "semantic-ui-react";
-import { useDispatch } from 'react-redux';
-import { doc, setDoc } from 'firebase/firestore';
-import { db } from "../../app/config/firebase";
-import { resetCheckoutId } from "../../app/store/CheckoutId";
-import { resetProvince } from "../../app/store/Province";
-import { resetShoppingFormState } from "../../app/store/ShoppingFormInput";
-import { resetCartItems } from "../Product/ProductItemSlices";
-import { useLocation } from 'react-router-dom';
 
 export default function FinishShopping() {
-  const dispatch = useDispatch();
-  const location = useLocation();
-
-  useEffect(() => {
-    const checkPaymentStatus = async () => {
-      // Get the orderCode and paymentLinkId from local storage
-      const orderCode = JSON.parse(localStorage.getItem('orderCode') || '');
-      const paymentLinkId = JSON.parse(localStorage.getItem('paymentLinkId') || '');
-
-      // Get the query parameters from the URL
-      const urlParams = new URLSearchParams(location.search);
-      const urlOrderCode = urlParams.get('orderCode');
-      const urlId = urlParams.get('id');
-      const urlStatus = urlParams.get('status');
-
-      if (String(orderCode) === urlOrderCode && String(paymentLinkId) === urlId && urlStatus === 'PAID') {
-        // Get the tempCartItems from local storage
-        const tempCartItems = JSON.parse(localStorage.getItem('tempCartItems') || '');
-        console.log(tempCartItems);
-
-        if (tempCartItems) {
-          await setDoc(doc(db, "đơn hàng đã chuyển khoản", paymentLinkId), tempCartItems);
-          dispatch(resetCartItems());
-          dispatch(resetShoppingFormState());
-          dispatch(resetProvince());
-          dispatch(resetCheckoutId());
-          localStorage.clear();
-        }
-      }
-    };
-
-    checkPaymentStatus();
-  }, [dispatch, location]);
 
   return (
     <Container className="thankyou" style={{marginTop: '7em', marginBottom: '5em', scale: '0.9'}}>
--- src/features/Product/ProductListItems.tsx
+++ src/features/Product/ProductListItems.tsx
@@ -1,39 +1,27 @@
-import { Grid, Container} from "semantic-ui-react";
+import { Grid, Container, Reveal, RevealContent, Image} from "semantic-ui-react";
 import AddToShoppingCart from "../Shopping Cart/AddToShoppingCart";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+
 
 export default function ProductListItems(props: any) {
-
-  const [hoveredImage, setHoveredImage] = useState<string | null>(null);
-
-  const navigate = useNavigate();
     
   return (
     <Grid centered>
         <Grid.Row>
           {props.products && props.products.map((products: any, index: number) => {
-            const isHovered = hoveredImage === products.name;
             return (
               <Grid.Column key={index} width={3} style={{ padding: '1vw', marginBottom: '2vh' }}>
                 <Container className="product">
-                <div className="image-container" onClick={() => navigate(`/sanpham/${products.id}`)} style={{cursor: 'pointer'}}>
-                  <img
-                    src={products.photoURL}
-                    alt={products.name}
-                    className={`product-image ${isHovered ? 'fade-out' : 'fade-in'}`}
-                  />
-                  <img
-                    src={products.photoURL2}
-                    alt={products.name}
-                    className={`product-image ${isHovered ? 'fade-in' : 'fade-out'}`}
-                    onMouseEnter={() => setHoveredImage(products.name)}
-                    onMouseLeave={() => setHoveredImage(null)}
-                  />
-                </div>
+                  <Reveal animated="fade">
+                    <RevealContent visible>
+                      <Image src={products.photoURL}/>
+                    </RevealContent>
+                    <RevealContent hidden>
+                      <Image src={products.photoURL2}/>
+                    </RevealContent>
+                  </Reveal>
                   <AddToShoppingCart productitems = {products}/>
                   <h2>{products.name}</h2>
-                  <p>{products.description}</p>
+                  <p>Mã: {products.id}</p>
                   <h3><strong>{products.price.toLocaleString()}</strong><u>đ</u></h3>
                 </Container>
               </Grid.Column>
