be08b321164321edda86425576fc4c7109ed8659
LyyHua <lyhuavanly@gmail.com>
2024-05-13 02:11:09 +0700

    Heard it make web work better ?

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          lodash: ['lodash'],
+          react: ['react', 'react-dom'],
+          semanticUI: ['semantic-ui-react'],
+        },
+      },
+    },
+  },
   server: {
     port: 3000
   },
